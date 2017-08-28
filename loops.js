47c48b22e27ba4cac2402b85afe285958ce61f7e
lglayat <glayatle@g.cofc.edu>
2017-08-28 15:59:33 -0400

    Done.

---
+++ loops.js
@@ -0,0 +1,35 @@
+
+
+function forLoop(arr){
+  let size = arr.length
+  for(let i = 0; i < 25; i++){
+    if (i === 1){
+      arr.push("I am 1 strange loop.")
+    } else {
+      arr.push("I am ${i} strange loops.")
+    }
+  }
+  return arr
+}
+
+
+function whileLoop(num){
+  while(num > 0){
+    console.log(num)
+    num -= 1
+  }
+  return 'done'
+}
+
+
+function doWhileLoop(array){
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do{
+     array = array.slice(1)
+  } while (array.length > 0 && maybeTrue())
+
+  return array
+}
